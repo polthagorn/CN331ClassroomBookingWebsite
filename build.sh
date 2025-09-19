84f360a0abc0b701e8e57c1de4700e5731ca228f
polthagorn <6610685262@cn330>
2025-09-20 06:13:19 +0700

    change build

--- build.sh
+++ build.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
- # Exit on error
- set-o errexit
- pip install--upgrade pip
- pip install-r requirements.txt
- python manage.py collectstatic--no-input
- python manage.py migrate
- python manage.py createsuperuser--username admin--email "your@email.com"-
-noinput || true
+# Exit on error
+set -o errexit
+
+pip install --upgrade pip
+pip install -r requirements.txt
+
+python manage.py collectstatic --noinput
+python manage.py migrate
+
+python manage.py createsuperuser \
+  --username admin \
+  --email "your@email.com" \
+  --noinput || true
